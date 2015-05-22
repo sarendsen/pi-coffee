28a3dafd45ddc578ab225cdf70b37b5471535512
Sjoerd Arendsen <subscribe@optixdesigns.com>
2015-05-22 10:49:18 +0200

    adding views

--- application/urls.py
+++ application/urls.py
@@ -1,14 +1,8 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from .views import *
+
 urlpatterns = patterns('',
-
+	url(r'^api/$', on_off_view, name='on_off_view'),
 )
-
-
-import RPi.GPIO as GPIO
-
-
-mode = GPIO.getmode()
-print "yo"
-print mode
--- application/views.py
+++ application/views.py
@@ -1,1 +1,38 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
+from rest_framework.decorators import api_view, throttle_classes
+import RPi.GPIO as GPIO
 
+# 17 = Sterk
+# 18 = Normaal
+# 27 = Sterkte
+# 22 = Aan/Uit
+
+mode = GPIO.getmode()
+print mode
+
+HIGH = 1
+LOW = 0
+
+
+@api_view(['GET'])
+def on_off_view(request):
+    if 'status' not in request.GET:
+        return Response()
+
+    requested_status = request.GET['status']
+
+    if requested_status == 'on':
+        GPIO.output(18, HIGH)
+    elif requested_status == 'off':
+        GPIO.output(18, LOW)
+
+    '''
+    if GPIO.input(channel):
+        print('Input was HIGH')
+    else:
+        print('Input was LOW')
+    '''
+
+    return Response({"message": "sup"})
