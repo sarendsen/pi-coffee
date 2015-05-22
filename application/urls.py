9708781a0ad33b1e4312d29c05828a92d06bf3a6
Sander van de Graaf <mail@svdgraaf.nl>
2015-05-22 12:40:30 +0200

    cleanup and added templates

--- application/urls.py
+++ application/urls.py
@@ -4,8 +4,9 @@ from django.contrib import admin
 from .views import *
 
 urlpatterns = patterns('',
-    url(r'^api/power/$', power, name='power_view'),
-    url(r'^api/expresso/$', coffee_strong, name='coffee_strong_view'),
-    url(r'^api/coffee/$', coffee_normal, name='coffee_normal_view'),
-    url(r'^api/strength/(?P<strength>\d+)/$', coffee_strength, name='coffee_strength_view'),
+	url(r'^$', index, name='index_view'),
+	url(r'^api/power/$', power, name='power_view'),
+	url(r'^api/espresso/$', coffee_strong, name='coffee_strong_view'),
+	url(r'^api/coffee/$', coffee_normal, name='coffee_normal_view'),
+	url(r'^api/strength/1$', coffee_normal, name='coffee_normal_view'),
 )
